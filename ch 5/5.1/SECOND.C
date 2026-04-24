13d0df47dd17bab17300b59ee4dcdffdd750bf6c
rabariprince510 <rabariprince927@gmail.com>
2026-04-24 16:40:39 +0530

    'control'

---
+++ ch 5/5.1/SECOND.C
@@ -0,0 +1,21 @@
+ #include<stdio.h>
+ #include<conio.h>
+
+ main()
+ {
+   int age;
+   clrscr();
+   printf("enter your age: ");
+   scanf("%d",&age);
+
+   if(age>=18)
+   {
+    printf("you can vote.");
+   }
+   else
+   {
+    printf("you can't vote.");
+   }
+   getch();
+ }
+
